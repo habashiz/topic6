2933634a349ace4796599cc6da1a6765de7471cf
habashi <zaidi.hakim.aftec@gmail.com>
2016-12-09 11:35:17 +0100

    Ajout du boucle while dans le script main <topic6> permetant d'automatiser la relance du programme

--- src/topic6.py
+++ src/topic6.py
@@ -7,19 +7,30 @@ import os, sys, stat
 from pathModule import *
 from wordModule import *
 
-#Instanciation de deux listes corespondant respectivement a la liste des fichier html dans le repertoire et de la liste des fichiers modifiés depuis notre méthode correspondante
-listFiles = list()
-listFilesModified = list()
+#bool indiquant l'execution du programme
+execution = True
+print("Ce programme permet d'entourer un mot avec une balise statique \n <span class=important/> sur un fichier html depuis un repertoire donnée")
+while(execution):
+	#Instanciation de deux listes corespondant respectivement a la liste des fichier html dans le repertoire et de la liste des fichiers modifiés depuis notre méthode correspondante
+	listFiles = list()
+	listFilesModified = list()
 
-directory = input("Veuillez indiquer un repertoire: ")
-#La méthode est importée depuis le module "pathModule" est permet de lister les fichiers html centenus dans le repertoire indiqué 
-listFiles = getFilesDirectory(directory)
-print(listFiles)
+	directory = input("Veuillez indiquer un repertoire: ")
+	#La méthode est importée depuis le module "pathModule" est permet de lister les fichiers html centenus dans le repertoire indiqué 
+	listFiles = getFilesDirectory(directory)
+	print(listFiles)
 
-mot = input("Veuillez indiquer le mot que vous voulez entourer de balise span class important: ")
+	mot = input("Veuillez indiquer le mot que vous voulez entourer de balise span class important: ")
 
-#La méthode est importée depuis le module "wordModule" est permet d'inserer une balise span class important sur du contenu html
-listFilesModified = makeImportantToWord(directory, listFiles, mot)
-print("Les fichiers contenant le mot et modifiés sont : ")
-print(listFilesModified)
-print("Une copie des fichiers initiaux est sauvegarder avec le nom initial du fichier avec l'extention .old sur le mème repertoire")
+	#La méthode est importée depuis le module "wordModule" est permet d'inserer une balise span class important sur du contenu html
+	listFilesModified = makeImportantToWord(directory, listFiles, mot)
+	if len(listFilesModified) != 0:
+		print("Les fichiers contenant le mot et modifiés sont : ")
+		print(listFilesModified)
+		print("Une copie des fichiers initiaux est sauvegarder avec le nom initial du fichier avec l'extention .old sur le mème repertoire")
+	else :
+		print("Aucun fichier ne contient le mot passé en parametre, par consequent aucun fichier n'a etait modifié")
+
+	exe = input("Si vous voulez reexecuter le programme taper O, si non taper N : ")
+	if exe == 'N':
+		execution = False	
