3f16cb05890f0b2b24c75fcbb5c28babe7809f45
habashi <zaidi.hakim.aftec@gmail.com>
2016-12-09 10:52:23 +0100

    Ajout du nom de l'etudiant et de sa promotion sur le script principal comme exigé

--- src/topic6.py
+++ src/topic6.py
@@ -1,6 +1,7 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+# ZAIDI Hakim
+# CDI 2ème année
+# Topic 6 : Modifier du HTML avec Python
+
 import os, sys, stat
 
 from pathModule import *
